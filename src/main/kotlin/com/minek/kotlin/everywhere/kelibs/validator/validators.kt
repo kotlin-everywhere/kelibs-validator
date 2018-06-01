cf10e14d6fd5d4845f5d62bb480fd06b7131409c
Park Sung Min <vamf12@gmail.com>
2018-06-01 14:05:11 +0900

    add predefined ifInvalidShortcut

--- src/main/kotlin/com/minek/kotlin/everywhere/kelibs/validator/validators.kt
+++ src/main/kotlin/com/minek/kotlin/everywhere/kelibs/validator/validators.kt
@@ -1,3 +1,5 @@
 package com.minek.kotlin.everywhere.kelibs.validator
 
 fun <T, E> ifInvalid(error: E, test: (T) -> Boolean): (T) -> List<E> = { if (test(it)) listOf(error) else listOf() }
+
+fun <T> ifInvalid(test: (T) -> Boolean): (T) -> List<String> = ifInvalid("Invalid", test)
--- src/test/kotlin/com/minek/kotlin/everywhere/kelibs/testValidators.kt
+++ src/test/kotlin/com/minek/kotlin/everywhere/kelibs/testValidators.kt
@@ -10,5 +10,9 @@ class TestValidators {
         val validator = ifInvalid<Boolean, String>("false") { !it }
         assertEquals(listOf(), validator(true))
         assertEquals(listOf("false"), validator(false))
+
+        val validator2 = ifInvalid<Boolean> { !it }
+        assertEquals(listOf(), validator2(true))
+        assertEquals(listOf("Invalid"), validator2(false))
     }
 }
