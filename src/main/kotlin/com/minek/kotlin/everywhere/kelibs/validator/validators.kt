88a3cc91da80f4107d81646569f38e69c0767a6c
Park Sung Min <vamf12@gmail.com>
2018-06-01 14:01:38 +0900

    refactoring

--- src/main/kotlin/com/minek/kotlin/everywhere/kelibs/validator/validators.kt
+++ src/main/kotlin/com/minek/kotlin/everywhere/kelibs/validator/validators.kt
@@ -1,5 +1,3 @@
 package com.minek.kotlin.everywhere.kelibs.validator
 
-fun <T, E> ifInvalid(error: E, test: (T) -> Boolean): (T) -> List<E> {
-    return { if (test(it)) listOf(error) else listOf() }
-}
+fun <T, E> ifInvalid(error: E, test: (T) -> Boolean): (T) -> List<E> = { if (test(it)) listOf(error) else listOf() }
