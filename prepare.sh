6e61bb326845cacf4d4f4eb3c3ac660c6d37f309
Park Sung Min <vamf12@gmail.com>
2018-06-01 11:58:20 +0900

    remove old source

--- prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "rootProject.name='kelibs-validator'\ninclude 'kelibs-validator-java', 'kelibs-validator-js'" > settings.gradle
-touch build.gradle
-kelibs-validator-java/gradlew wrapper
